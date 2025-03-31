daff5b93e2b8700cfcd19694e29988d7414e79bd
정도영 <sonatine789@naver.com>
2025-04-01 05:25:26 +0900

    solve: 25083 - 새싹

---
+++ Do0/week1/Q_25083_B5_100ms.java
@@ -0,0 +1,10 @@
+public class Q_25083_B5_100ms {
+	public static void main(String[] args){
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
