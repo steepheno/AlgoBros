4fa49ec12b07d68c502183953bfa2dde7048f43e
mins-git <m2insapp@gmail.com>
2025-06-21 18:07:46 +0900

    solve: 2557 - Hello World

---
+++ Min/week1/HellowWorld_2557.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HellowWorld_2557 {
+    public static void main(String[] args) throws IOException {
+
+//        String input = "src/input.txt";
+//        BufferedReader br = new BufferedReader(new FileReader(input));
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Hellow World!");
+
+    }
+}
